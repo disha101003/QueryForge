208ffae207cd9fb4686de6704c849d934de5b4d3
Disha Sanjay Maheshwari <dsmahesh@eceprog4.ecn.purdue.edu>
2024-09-08 20:00:58 -0400

    Added unit test for testing code

---
+++ src/tests/unit/test_app.py
@@ -0,0 +1,25 @@
+import pytest
+import sys
+import os
+
+# Add the src directory to the PYTHONPATH
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../backend/app')))
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home_page(client):
+    """Test the home page (/) route."""
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Login" in response.data  # Adjust this according to your homepage content
+
+def test_some_route(client):
+    """Test another route (/some_route) in your app."""
+    response = client.get('/signup')
+    assert response.status_code == 200
+    assert b"Sign Up" in response.data  # Adjust to match expected output
